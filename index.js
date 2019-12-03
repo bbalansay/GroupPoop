faadaa49dccb8da70a1c4c285ab85cf238ae177a
Alex <alwong34@uw.edu>
2019-12-02 22:49:00 -0800

    patch review

--- index.js
+++ index.js
@@ -34,8 +34,35 @@ function checkAuth(req, res) {
 /*
     RESOURCE PATH: /review
     SUPPORTED METHODS:
-    - GET
     - POST
+*/
+app.post("/review/:reviewID", async (req, res) => {
+    checkAuth(req, res)
+    let db;
+    let reviewID = req.params.reviewID;
+
+    try {
+        db = await getDB()
+        let user = JSON.parse(req.get("X-User"))
+
+        let reviewJSON = req.body;
+        let rows = await db.query(`
+            INSERT INTO Review (user_id, bathroom_id, content, time)
+            VALUES (${user.id}, ${reviewJSON.bathroom_id}, ${reviewJSON.content}, NOW())
+        `)
+
+        if (db) db.end();
+        return res.status(200).json(channels);
+    } catch {
+        if (db) db.end();
+        return res.status(500).json( {"error" : err.message })
+    }
+})
+
+/*
+    RESOURCE PATH: /review:reviewID
+    SUPPORTED METHODS:
+    - GET
     - PATCH
     - DELETE
 */
@@ -64,20 +91,42 @@ app.get("/review/:reviewID", async (req, res) => {
     }
 })
 
-app.post("/review/:reviewID", async (req, res) => {
+app.patch("/review/:reviewID", async (req, res) => {
     checkAuth(req, res)
     let db;
     let reviewID = req.params.reviewID;
 
     try {
-        db = await getDB()
+        db = await getDB();
         let user = JSON.parse(req.get("X-User"))
 
-        let reviewJSON = req.body;
-        let rows = await db.query(`
-            INSERT INTO Review (content, time)
-            VALUES ('${reviewJSON.content}, NOW())
+        // check review exists
+        let result = await db.query(`
+            SELECT * FROM Review
+            WHERE review_id = ${reviewID}
         `)
+        if (result.length != 1) {
+            return res.status(403).send("Review does not exist")
+        }
+
+        // check to see if user is author of review
+        if (user.id != result[0].user_id) {
+            return res.status(40).send("Your are not the creator of this review!")
+        }
+
+        if (req.body.content) {
+            await db.query(`
+                UPDATE Review SET content = '${req.body.contnet}'
+                WHERE review_id = ${reviewID}
+            `)
+            await db.query(`
+                UPDATE Review SET time = NOW()
+                WHERE id = ${reviewID}
+            `)
+        }
+    } catch {
+        if (db) db.end();
+        return res.status(500).json({"error": err.message})
     }
 })
 
