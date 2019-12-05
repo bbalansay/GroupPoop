45ab64ac9f4656c90385cfc2c801f359f521ad0c
Bradley Balansay <bradleybalansay@gmail.com>
2019-12-04 18:20:33 -0800

    asdljkfhsgf

--- clients/static/js/addEvent.js
+++ clients/static/js/addEvent.js
@@ -1,37 +1,37 @@
 window.onload = () => {
+  setTimeout(() => {
+    $("#fav").click((e) => {
+      e.preventDefault()
 
-  $("#fav").click((e) => {
-    e.preventDefault()
-
-    console.log("???")
-
-    fetch("https://api.grouppoop.icu/favorites/" + params.get(id), {
-      method: 'POST',
-      headers: {
-        'Authorization': sessionStorage.auth
-      }
+      fetch("https://api.grouppoop.icu/favorites/" + params.get("id"), {
+        method: 'POST',
+        headers: {
+          'Authorization': sessionStorage.auth
+        }
+      })
+        .then(checkStatus)
+        .catch(console.log)
     })
-      .then(checkStatus)
-      .then(console.log(resp.json()))
-      .catch(console.log)
-  })
 
-  $("#btnRev").click((e) => {
-    e.preventDefault()
+    $("#btnRev").click((e) => {
+      e.preventDefault()
 
-    fetch("https://api.grouppoop.icu/bathroom/" + params.get(id) + "/review", {
-      method: 'POST',
-      body: JSON.stringify({
-        Score: $("#score").val(),
-        Content: $("#content").val()
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': sessionStorage.auth
-      }
-    })
-      .then(checkStatus)
-      .then(console.log(resp.json()))
-      .catch(console.log)
-    })
+      fetch("https://api.grouppoop.icu/bathroom/" + params.get("id") + "/review", {
+        method: 'POST',
+        body: JSON.stringify({
+          Score: $("#score").val(),
+          Content: $("#content").val()
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': sessionStorage.auth
+        }
+      })
+        .then(checkStatus)
+        .then(resp => resp.json())
+        .then(console.log)
+        .then(window.location.reload())
+        .catch(console.log)
+      })
+  }, 1000)
 }
